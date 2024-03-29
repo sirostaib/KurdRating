099dec36e515b90cd0685a6e99857938399ee161
Caesar <sirostaib@gmail.com>
2024-03-29 14:56:32 +0300

    fix: :memo: updated the main tab bar view controller

--- KurdRating/MainTapBarViewController.swift
+++ KurdRating/MainTapBarViewController.swift
@@ -14,10 +14,23 @@ class MainTapBarViewController: UITabBarController {
         view.backgroundColor = .systemCyan
         
         let vc1 = UINavigationController(rootViewController: HomeVC())
-        let vc2 = UINavigationController(rootViewController: SearchVC())
-        let vc3 = UINavigationController(rootViewController: DownloadsVC())
+        let vc2 = UINavigationController(rootViewController: UpcomingVC())
+        let vc3 = UINavigationController(rootViewController: SearchVC())
+        let vc4 = UINavigationController(rootViewController: DownloadsVC())
         
-        setViewControllers([vc1, vc2, vc3], animated: true)
+        vc1.tabBarItem.image = UIImage(systemName: "popcorn.fill")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "sparkle.magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "square.and.arrow.down.fill")
+        
+        vc1.tabBarItem.title = "Home"
+        vc2.tabBarItem.title = "Upcoming"
+        vc3.tabBarItem.title = "Search"
+        vc4.tabBarItem.title = "Downloads"
+        
+        tabBar.tintColor = .label
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
 
 
